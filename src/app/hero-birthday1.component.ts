a4632f8e2e711a3085f20c3e6064f061a45f09d4
Wanasak <u510610433@gmail.com>
2017-03-01 15:47:52 +0700

    parameterizing a pipe

--- src/app/hero-birthday1.component.ts
+++ src/app/hero-birthday1.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'hero-birthday1',
     template: `
-        <p>The hero's birthday is {{ birthday | date }}</p>
+        <p>The hero's birthday is {{ birthday | date:format }}</p>
+        <button (click)="toggleFormat()">Toggle Date Format</button>
     `
 })
 export class HeroBirthday1Component {
     birthday = new Date(1988, 3, 15);
+    toggleDateFormat: boolean = true;   // true is shortDate
+
+    get format() {
+        return this.toggleDateFormat ? 'shortDate' : 'fullDate';
+    }
+    toggleFormat() {
+        this.toggleDateFormat = !this.toggleDateFormat;
+    }
 }
