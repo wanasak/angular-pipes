5810335c2d6bbe94784ebf37b20f9f3a2fe3c3ea
Wanasak <u510610433@gmail.com>
2017-03-01 15:43:37 +0700

    build-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <p>{{ title | uppercase }}</p>
+    <p>{{ title | lowercase }}</p>
+    <p>{{ myNum | currency }}</p>
+    <p>{{ myNum | percent:'3.2-2' }}</p>
     <hero-birthday1></hero-birthday1>
   `,
 })
-export class AppComponent  { title = 'Angular Pipe Tutorial'; }
+export class AppComponent {
+  title = 'Angular Pipe Tutorial';
+  myNum: number = 0.1415927;
+}
